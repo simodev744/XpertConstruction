c2df69eeb76a763f7f7cd7a3a07225a248a94406
simodev744 <ammar.mohamed744@gmail.com>
2025-03-18 23:47:11 +0000

    creation les tables dans la base de donnee a partir du fichier db/databaseTable.txt et etablissment de la connection avec base de donnee

---
+++ src/main/java/org/example/xpertaconstruction/db/DBConnection.java
@@ -0,0 +1,20 @@
+package org.example.xpertaconstruction.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/constructionexpert";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL jdbc n'est pas trouve", e);
+        }
+    }
+}
